2854e6f3b56ab0a7ccb5b92907f613e4077fd6d5
Surya <teja.surya18@gmail.com>
2024-12-01 11:15:03 +0530

    main: Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from assistant import create_app
 
 app = Flask(__name__, instance_relative_config=True)
 
+create_app(app)
+
 if __name__ == "__main__":
-    create_app(app)
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
